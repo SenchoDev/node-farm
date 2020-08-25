02282dcbdf42ae0efbd866d8960084355b9e3949
Senadin <beganovicsenadin24@gmail.com>
2020-08-25 13:31:02 +0200

    started using routing in app with http module

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 const fs = require('fs');
+const http = require('http');
+const url = require('url');
+
+/////////////////////////////////////////////
+// FILES
+////////////////////////////////////////////
+
 // SYNCHROUNOUS => BLOCKING
 
 // const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
@@ -8,15 +15,39 @@ const fs = require('fs');
 
 
 // ASYNCHRONOUS => NON-BLOCKING
-fs.readFile('./txt/start.txt', 'utf-8', (error, data1) => {
-  fs.readFile(`./txt/${data1}.txt`, 'utf-8', (error, data2) => {
-    console.log(data2);
-    fs.readFile(`./txt/append.txt`, 'utf-8', (error, data3) => {
-      console.log(data3);
-      fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
-        console.log('Your file has been written')
-      })
+// fs.readFile('./txt/start.txt', 'utf-8', (error, data1) => {
+//   fs.readFile(`./txt/${data1}.txt`, 'utf-8', (error, data2) => {
+//     console.log(data2);
+//     fs.readFile(`./txt/append.txt`, 'utf-8', (error, data3) => {
+//       console.log(data3);
+//       fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
+//         console.log('Your file has been written')
+//       })
+//     });
+//   });
+// });
+// console.log('hey')
+
+/////////////////////////////////////////////
+// SERVER
+////////////////////////////////////////////
+
+const server = http.createServer((req, res) => {
+  const pathName = req.url;
+  if(pathName === '/' || pathName === '/overview'){
+    res.end('This is the OVERVIEW')
+  } else if(pathName === '/product') {
+    res.end('This is the PRODUCT')
+  } else {
+    res.writeHead(404, {
+      'Content-type': 'text/html',
+      'my-own-header': 'hello-world'
     });
-  });
-});
-console.log('hey')
+    res.end(`<h1>Page not found!</h1>`);
+  }
+  res.end('Hello from the server!');
+})
+
+server.listen(8000, '127.0.0.1', () => {
+  console.log('Listentning to requests on port 8000')
+})
