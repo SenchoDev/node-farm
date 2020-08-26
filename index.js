90c55a01f1dede9552f8561abaccb3e5d43e3951
Senadin <beganovicsenadin24@gmail.com>
2020-08-26 14:11:36 +0200

    built very simple api

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const fs = require('fs');
-const http = require('http');
-const url = require('url');
+const fs = require("fs");
+const http = require("http");
+const url = require("url");
 
 /////////////////////////////////////////////
 // FILES
@@ -12,7 +12,6 @@ const url = require('url');
 // console.log(textIn);
 // const textOut = `This is what we konw about the avocado : ${textIn}.Created on ${Date.now()}`;
 // fs.writeFileSync('./txt/output.txt', textOut)
-
 
 // ASYNCHRONOUS => NON-BLOCKING
 // fs.readFile('./txt/start.txt', 'utf-8', (error, data1) => {
@@ -32,22 +31,28 @@ const url = require('url');
 // SERVER
 ////////////////////////////////////////////
 
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
+
 const server = http.createServer((req, res) => {
   const pathName = req.url;
-  if(pathName === '/' || pathName === '/overview'){
-    res.end('This is the OVERVIEW')
-  } else if(pathName === '/product') {
-    res.end('This is the PRODUCT')
+  if (pathName === "/" || pathName === "/overview") {
+    res.end("This is the OVERVIEW");
+  } else if (pathName === "/product") {
+    res.end("This is the PRODUCT");
+  } else if (pathName === "/apis") {
+    res.writeHead(200, { "Content-type": "application/json" });
+    res.end(data);
   } else {
     res.writeHead(404, {
-      'Content-type': 'text/html',
-      'my-own-header': 'hello-world'
+      "Content-type": "text/html",
+      "my-own-header": "hello-world",
     });
     res.end(`<h1>Page not found!</h1>`);
   }
-  res.end('Hello from the server!');
-})
+  res.end("Hello from the server!");
+});
 
-server.listen(8000, '127.0.0.1', () => {
-  console.log('Listentning to requests on port 8000')
-})
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listentning to requests on port 8000");
+});
